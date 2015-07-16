0359fc2a0c39765e8c636fff397fd612d4b43151
steveoh <sgourley@utah.gov>
2015-07-16 12:37:13 -0600

    refactor wqp into own test module

--- tests/test_dbseeder.py
+++ tests/test_dbseeder.py
@@ -8,8 +8,7 @@ test the dbseeder module
 '''
 
 import unittest
-from dbseeder.dbseeder import Seeder, WqpProgram
-from os.path import join, basename
+from dbseeder.dbseeder import Seeder
 
 
 class TestDbSeeder(unittest.TestCase):
@@ -44,29 +43,3 @@ class TestParseSourceArgs(unittest.TestCase):
         self.assertEqual(self.patient._parse_source_args('WQP '), ['WQP'])
         self.assertEqual(self.patient._parse_source_args(' WQP'), ['WQP'])
         self.assertEqual(self.patient._parse_source_args(' WQP '), ['WQP'])
-
-
-class TestWqpProgram(unittest.TestCase):
-    def setUp(self):
-        self.test_get_files_folder = join('tests', 'data', 'WQP', 'get_files')
-        self.patient = WqpProgram(self.test_get_files_folder)
-
-    def test_get_files_finds_files(self):
-        expected_results = [
-            'sample_chemistry.csv',
-            'sample_chemistry2.csv',
-            'test.csv'
-        ]
-
-        expected_stations = [
-            'sample_stations.csv'
-        ]
-
-        results_folder = map(basename, self.patient._get_files(self.patient.results_folder))
-        stations_folder = map(basename, self.patient._get_files(self.patient.stations_folder))
-
-        self.assertItemsEqual(results_folder, expected_results)
-        self.assertItemsEqual(stations_folder, expected_stations)
-
-    def test_get_samples_for_id_returns_correnct_list(self):
-        csv_file = None
